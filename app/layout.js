7f81d89b00543bee573d137c68dae35f12bce08a
rynekpracownika <robertgodekk@gmail.com>
2026-05-30 13:39:19 +0200

    feat: PWA manifest, ikony, meta tagi

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,14 @@ import CookieBanner from "./components/CookieBanner";
 export default function RootLayout({ children }) {
   return (
     <html lang="pl" translate="no">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1A73E8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="RynekPracy" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.svg" />
+      </head>
       <body>
         <SessionProvider>
           {children}
